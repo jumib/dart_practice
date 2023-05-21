df213536ded0f1dbcfbf213a3bd34e56e6f67e99
jumib <baekjumi@naver.com>
2023-05-21 15:17:46 +0900

    14. Optional Positional Parameters

--- main.dart
+++ main.dart
@@ -1,12 +1,10 @@
-// 중괄호 추가
-// default value 추가 (parameter 없어도 호출 가능)
-// 또는 required 추가 (반드시 파라미터 넘겨받아야 하는 경우)
-String sayHello ({required String name, required int age, required String contry }) {
-  return "hi $name, you are $age, from $contry";
+// named parameter 안쓰고 필수값 아닐 경우
+String sayHello (String name, int age, [String? country = 'korea'] ) {
+  return "hi $name, you are $age, from $country";
 }
 
 void main() {
-  // 순서에 상관 없이 parameter name만 적어주면 됨
-  print(sayHello(name: 'jumi', contry: 'korea', age: 11));
+  // 순서에 상관 있음 => Positional parameter
+  print(sayHello('jumi', 11));
 
 }
