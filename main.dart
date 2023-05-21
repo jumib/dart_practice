e149f8e1d307cd0396a116956c24db82942f3f5b
jumib <baekjumi@naver.com>
2023-05-21 14:20:13 +0900

    10. Maps

--- main.dart
+++ main.dart
@@ -1,13 +1,19 @@
 void main() {
-  var oldFriends = ['minji', 'haerin'];
-  var newFriends = [
-    'daniel', 
-    'hanni', 
-    'hyein',
-    for (var friend in oldFriends) "S2 $friend",  
+  // object = any
+  // object는 기본적으로 어떤 자료형이든 될 수 있다
+  // key와 value를 가지는 구조로 obj를 만들 때, 그것이 특정 형태를 가질 때 (: get api data 같은)
+  // Map 보다 Dart의 class를 사용하는 것이 낫다
+
+  List<Map<String, Object>> players = [
+    {
+      'name': 'jumi',
+      'xp': 1999.99,
+      'superPower': false
+    },
+    {
+      'name': 'jumi',
+      'xp': 1999.99,
+      'superPower': false
+    }
   ];
-  // for (var friend in oldFriends) {
-  //  newFriends.add()
-  // }
-  print(newFriends);
 }
