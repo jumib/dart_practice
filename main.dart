f3797fe91ca7b14484d9ce3fe530fd1cecbf3b9f
jumib <baekjumi@naver.com>
2023-05-21 15:35:30 +0900

    16. Typedef

--- main.dart
+++ main.dart
@@ -1,13 +1,13 @@
-// left ?? right
-// left가 null 이면 right 반환
-// null  아니면 left 반환
+// 자료형에 사용자가 원하는 alias를 붙일 수 있게 해줌
 
+typedef ListOfInts = List<int>;
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversedList = list.reversed.toList();
+  return reversedList;
+}
 
 void main() {
-  String? name;
-  name ?? 'jumi';
-  name = null;
-  name ?? 'another';
-  print(name);
+  print(reverseListOfNumbers([1,2,3]));
 
 }
